674b399308ecd06f4e4bdba50223caa30c4aa996
YAOSHUNKI <r121173@g.kobe-kosen.ac.jp>
2026-08-24 09:53:44 +0900

    add squat only task

--- source/unitree_rl_lab/unitree_rl_lab/tasks/pickup_carry/robots/g1/29dof/__init__.py
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/pickup_carry/robots/g1/29dof/__init__.py
@@ -26,3 +26,24 @@ gym.register(
         "rsl_rl_cfg_entry_point": "unitree_rl_lab.tasks.pickup_carry.agents.rsl_rl_ppo_cfg:G1PickupCarryPPORunnerCfg",
     },
 )
+
+# ----- SquatOnly ------------------------------------------------
+gym.register(
+    id="Unitree-G1-29dof-SquatOnly",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": "unitree_rl_lab.tasks.pickup_carry.robots.g1.29dof.squat_only_env_cfg:G1SquatOnlyEnvCfg",
+        "rsl_rl_cfg_entry_point": "unitree_rl_lab.tasks.pickup_carry.agents.rsl_rl_ppo_cfg:G1PickupCarryPPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="Unitree-G1-29dof-SquatOnly-Play",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": "unitree_rl_lab.tasks.pickup_carry.robots.g1.29dof.squat_only_env_cfg:G1SquatOnlyEnvCfg_PLAY",
+        "rsl_rl_cfg_entry_point": "unitree_rl_lab.tasks.pickup_carry.agents.rsl_rl_ppo_cfg:G1PickupCarryPPORunnerCfg",
+    },
+)
