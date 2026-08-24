3c99301ef606f35f1c85ff7b6724288064f81eb3
YAOSHUNKI <r121173@g.kobe-kosen.ac.jp>
2026-08-24 09:26:38 +0900

    change init.py

--- source/unitree_rl_lab/unitree_rl_lab/tasks/pickup_carry/robots/g1/29dof/__init__.py
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/pickup_carry/robots/g1/29dof/__init__.py
@@ -22,7 +22,7 @@ gym.register(
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": "unitree_rl_lab.tasks.pickup_carry.robots.g1.29dof.pickup_carry_env_cfg:G1PickupCarryEnvCfg_PLAY",
+        "play_env_cfg_entry_point": "unitree_rl_lab.tasks.pickup_carry.robots.g1.29dof.pickup_carry_env_cfg:G1PickupCarryEnvCfg_PLAY",
         "rsl_rl_cfg_entry_point": "unitree_rl_lab.tasks.pickup_carry.agents.rsl_rl_ppo_cfg:G1PickupCarryPPORunnerCfg",
     },
 )
